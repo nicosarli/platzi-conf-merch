76a36dae3f407433fa400e6192bf83d46275b50f
nicosarli <nico_sarli@hotmail.com>
2021-07-28 18:50:07 -0300

    customHook terminado y aplicando el hook useContext para pasar dicho customHook a toda la aplicacion

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -10,26 +10,32 @@ import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
 import '../styles/components/App.css';
 import Layout from '../components/Layout';
+import AppContext from '../context/AppContex';
+import useInitialState from '../hooks/useInitialState';
 
 const App = () => {
+  const initialState = useInitialState();
+
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
 
-          <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout" component={Checkout} />
 
-          <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/information" component={Information} />
 
-          <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/payment" component={Payment} />
 
-          <Route exact path="/checkout/success" component={Success} />
+            <Route exact path="/checkout/success" component={Success} />
 
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
