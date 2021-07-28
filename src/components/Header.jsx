ad736669d2074cb7367aac5c1b7672bd76b84651
nicosarli <nico_sarli@hotmail.com>
2021-07-28 18:37:46 -0300

    Poniendole iconos a Checkout y a Information, tambien creando un context y un customHook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css';
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header-tittle">PlatziConf Merch</h1>
-      <div className="Header-checkout">Checkout</div>
+      <h1 className="Header-tittle">
+        <Link to="/">PlatziConf Merch</Link>
+      </h1>
+      <div className="Header-checkout">
+        <Link to="/checkout">
+          {' '}
+          <i className="fas fa-shopping-basket fa-2x" tittle="Checkout" />
+        </Link>
+      </div>
     </div>
   );
 };
--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Information.css';
 
 const Information = () => {
@@ -30,8 +31,12 @@ const Information = () => {
           </form>
         </div>
         <div className="Information-buttons">
-          <div className="Information-back">Regresar</div>
-          <div className="Information-next">Pagar</div>
+          <div className="Information-back">
+            <Link to="/">Regresar</Link>
+          </div>
+          <div className="Information-next">
+            <Link to="/checkout/payment">Pagar</Link>
+          </div>
         </div>
       </div>
       <div className="Information-sidebar">
